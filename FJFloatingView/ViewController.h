a7d0e09b48c5a7c5736d901d60c4d93cb0b6ce33
方金峰 <fjf@fjfdeimac.local>
2016-06-07 09:04:16 +0800

    Initial Commit

---
+++ FJFloatingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FJFloatingView
+//
+//  Created by fjf on 16/6/7.
+//  Copyright © 2016年 fjf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
